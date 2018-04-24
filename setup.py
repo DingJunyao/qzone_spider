eb10d5afe7be9fb08b0c3633ca4028a9fa4b5a42
DingJunyao <dingjunyao0703@163.com>
2018-04-24 20:44:12 +0800

    " to '

--- setup.py
+++ setup.py
@@ -10,12 +10,13 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name="qzone_spider",
-      version="1.0.0a1",
-      description="Spider of Qzone",
+setup(name='qzone_spider',
+      version='1.0.0a1',
+      description='Spider of Qzone',
       long_description=long_description,
-      author="Ding Junyao",
-      author_email="dingjunyao0703@163.com",
+      long_description_content_type='text/markdown',
+      author='Ding Junyao',
+      author_email='dingjunyao0703@163.com',
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Environment :: Console',
@@ -39,9 +40,9 @@ setup(name="qzone_spider",
       keywords='spider Qzone QQ SNS',
       packages=find_packages(exclude=['contrib', 'docs', 'tests']),
       entry_points={
-        "console_scripts": [
-            "qzone_spider = qzone_spider.spider:main",
-            "qzone-spider = qzone_spider.spider:main",
+        'console_scripts': [
+            'qzone_spider = qzone_spider.spider:main',
+            'qzone-spider = qzone_spider.spider:main',
         ],
       },
       python_requires='>=3.6',
