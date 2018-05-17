32be46c8f5e4ebc46bff7c95947af8c34ceaf330
DingJunyao <dingjunyao0703@163.com>
2018-05-17 12:01:43 +0800

    add rough_spider for class

--- qzone_spider/class.py
+++ qzone_spider/class.py
@@ -38,10 +38,10 @@ class QzoneSpider(object):
             return -1
         else:
 
-            catch_time, msg = get_json.get_fine_json('490424586', '4f8281307554d9573e2c0200', self.__cookies,
-                                                     self.__gtk, self.__qzonetoken, 1, 10)
-            """catch_time, _, msg = get_json.get_rough_json(self.spider_qq, 0, 1, 10,
-                                             self.__cookies, self.__gtk, self.__qzonetoken, 1, 0)"""
+            """catch_time, msg = get_json.get_fine_json('490424586', '4f8281307554d9573e2c0200', self.__cookies,
+                                         self.__gtk, self.__qzonetoken, 1, 10)"""
+            catch_time, _, msg = get_json.get_rough_json(self.spider_qq, 0, 1, 10,
+                                 self.__cookies, self.__gtk, self.__qzonetoken, 1, 0)
             if msg == -1 or msg == -2:
                 logger.error('Can not get data with current log info')
                 return 1
@@ -94,24 +94,39 @@ class QzoneSpider(object):
                 spider_quantity = 20
             else:
                 spider_quantity = sub_quantity
-            r_catch_time, end_order, rough_json = get_json.get_rough_json(qq, start, spider_quantity, replynum,
+            r_catch_time, end_order, rough_json = get_json.get_rough_json(qq, end_order, spider_quantity, replynum,
                                                                           self.__cookies, self.__gtk, self.__qzonetoken,
                                                                           self.get_rough_json_try_time, self.error_wait)
+            rough_json_list.append({'catch_time': r_catch_time, 'msglist': rough_json})
             if r_catch_time == 0 and end_order == -1:
                 break
             end_order += 1
+        return rough_json_list
+
+    def rough_spider(self, qq, start=0, quantity=20, replynum=10):
+        rough_data = []
+        rough_json_list = self.get_rough_json(qq, start, quantity, replynum)
+        for rough_json_set in rough_json_list:
+            for i in range(len(rough_json_set['msglist'])):
+                rough_data_item = json_parse.rough_json_parse(rough_json_set['msglist'], i,
+                                                              rough_json_set['catch_time'], self.do_emotion_parse)
+                rough_data.append(rough_data_item)
+        return rough_data
+
+    def get_fine_json(self, qq, tid):
         pass
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s] %(name)s: %(levelname)s: %(message)s')
+    logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(name)s: %(levelname)s: %(message)s')
     logger = logging.getLogger(__name__)
     sp = QzoneSpider(spider_qq='***REMOVED***', spider_qq_password='***REMOVED***', scan=False, do_emotion_parse=True,
                      login_try_time=2, get_rough_json_try_time=2, get_fine_json_try_time=2, login_wait=3, scan_wait=20,
                      spider_wait=5, error_wait=600, proxy=None, debug=False)
-    user_cookies = {'ptisp': 'ctc', 'x-stgw-ssl-info': '86581239788db2cee2662917d834f2c0_0.332_1526476773.744_1_h2_N_Y_TLSv1.2_ECDHE-RSA-AES128-GCM-SHA256_19500_0', '_qz_referrer': 'qzone.qq.com', 'pgv_pvi': '70567936', 'uin': 'o0***REMOVED***', 'pgv_si': 's8204765184', 'skey': '@moAOm0BTz', 'pt2gguin': 'o0***REMOVED***', 'RK': 'AbiQZuzwR0', 'ptcz': 'ca602c5199ad4fd268cb417efcd03ff33e83adf98658c6aea56a040f9c7f1782', 'p_uin': 'o0***REMOVED***', 'pt4_token': 'MwOkoF2pC8TiDPHRh1xVUSyOqtGCLXhzuczJURV5GnU_', 'p_skey': 'tzqNdYoVNp8OyGb4NAlLgXsIoBmnJp6x*CTnZLPEj2Q_'}
-    user_gtk = 1131313531
-    user_qzonetoken = '9ba738e70497216bf1e57475ff84129d3e9e979edea0e5263fd965d343628f343ab5c6286f360fbeb1d1b837d0cb'
+    user_cookies = {'ptisp': 'ctc', 'x-stgw-ssl-info': '9cd3acb701142a607cafc364b8c375fe_0.396_1526526994.098_1_h2_N_Y_TLSv1.2_ECDHE-RSA-AES128-GCM-SHA256_18000_0', '_qz_referrer': 'qzone.qq.com', 'pgv_pvi': '4293600256', 'uin': 'o0***REMOVED***', 'pgv_si': 's7856120832', 'skey': '@J8H3XWAR0', 'pt2gguin': 'o0***REMOVED***', 'RK': 'CTiQdszhS0', 'ptcz': '1b0b1adb505fcdcb483cbfeb78dff16fd0105074f9297c2bc1d3ea47d096455a', 'p_uin': 'o0***REMOVED***', 'pt4_token': 'gf5jVtIRD3Gp1AV00y3kJdmvypYwED66S0wpV5s4X8g_', 'p_skey': '2-MUDgXfcUNIJuQ6BdEiNL*hHoa5C88hhEoby-X7UPA_'}
+    user_gtk = 110424484
+    user_qzonetoken = '663114793f1c147e9cd85ccfb32b80414025b4c005adfc9fd9fae3b3512645f258514df743c2885ab9bbf059424a4b8f'
     sp.set_login_info(user_cookies, user_gtk, user_qzonetoken)
-    # sp.login()
+    """sp.login()"""
     print(sp.status)
+    print(sp.rough_spider('***REMOVED***', 0, 63))
